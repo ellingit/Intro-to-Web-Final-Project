cd6d9c122df11632098f020f76201d44f2d5c2ae
ellingit <joshuatheellington@gmail.com>
2014-03-16 20:39:34 -0600

    commitron

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('#dialog').css('height', '0');
 	var $images = $('section article img');
 	$images.click(function(){
+		window.scrollTo(0,0);
 		$('#dialog').dialog({
 			open: function(event, ui) {
 				$("html").css({ overflow: 'hidden' })
